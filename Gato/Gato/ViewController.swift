3e5bc95a4e45c9fa8dbcc42bd15c995c460c9bf7
Alumno <alumno@GDL-MACING10.local>
2025-03-26 08:41:15 -0600

    GatoInicial

---
+++ Gato/Gato/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gato
+//
+//  Created by Alumno on 26/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
